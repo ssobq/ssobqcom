07ed6199fed372e0951de7941e77660a8c53fd0d
ssobq <user.name>
2023-07-10 14:13:55 -0500

    Actualizacion contacto

--- contacto.php
+++ contacto.php
@@ -15,12 +15,12 @@
 
     <!-- Metadatos Informacion-->
     <meta name="description" content="Contacto de SSO - CRC servicios de salud ocupacional">
-    
+
     <meta name="keywords" content="SSO, sso, Sso, Servicios de salud ocupacional, SSO - CRC Servicios de Salud Ocupacional, Servicios de Salud Ocupacional, CRC, crc Salud Ocupacional, seguridad en el trabajo, sst, SST, 
     medicina en el trabajo, ssobq, ssobqcom, salud en el trabajo, sso barranquilla, salud ocupacional barranquilla, sso Barranquilla, salud ocupacional Barranquilla">
-    
+
     <link rel="canonical" href="https://www.ssobq.com/">
-    
+
     <meta property="og:locale" content="es_ES">
     <meta property="og:site_name" content="SSO - CRC Servicios de Salud Ocupacional">
     <meta property="og:type" content="website">
@@ -29,9 +29,9 @@
     <meta property="og:url" content="https://www.ssobq.com/">
     <meta property="og:image" content="https://www.ssobq.com/img/logo.png">
     <meta property="og:image:secure_url" content="https://www.ssobq.com/img/logo.png">
-    
-    
-    
+
+
+
 
     <!--Google search-->
     <meta name="google-site-verification" content="PMbjD96NUeb_WmZZaNueqxxDQiH_kU5IXAGHMon5BQ0" />
@@ -57,40 +57,24 @@
         <div class="row">
 
             <div class="col-lg-6">
-
                 <br>
                 <div class="row">
                     <div class="col-lg-12">
-                        <h3 class="font-weight-bold colorAzul">SEDE PRINCIPAL</h3>
+                        <h2 class="font-weight-bold colorAzul">SEDE PRINCIPAL</h2>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-lg-12">
-                        <p>
-                            <br>Cra. 49 # 74 - 77/91
-                            <br>Barrio: Las Americas
-                            <br>Barranquilla / Atlantico / Colombia
-                        </p>
+
+                        <br>
+                        <h5>Cra. 49 # 74 - 77/91</h5>
+                        <h5>Barrio: Las Americas</h5>
+                        <h5>Barranquilla, Atlántico - Colombia</h5>
+
                     </div>
                 </div>
                 <br>
-                <div class="row">
 
-                    <div class="col-lg-3">
-                        <img class="img-fluid rounded center" src="img/contacto/SSOWhatssap.jpg">
-                    </div>
-
-                    <div class="col-lg-9">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <h3 class="font-weight-bold colorAzul">AGENDA TU CITA</h3>
-                            </div>
-                            <div class="col-lg-12">
-                                <a href="tel:+573135464664" style="color:black;"> (313) 546 4664</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
 
             <div class="col-lg-6">
@@ -116,57 +100,81 @@
                     </div>
                 </div>
                 <div class="row justify-content-center">
-                    <div class="col-lg-2"></div>
-                    <div class="col-lg-8">
-                        <table class="table ">
-                            <thead>
-                                <tr class="align-self-center text-center ">
-                                    <th scope="col">
-                                        <h5 class="colorAzul font-weight-bold">Tipo de Solicitud</h5>
-                                    </th>
-                                    <th scope="col">
-                                        <h5 class="colorAzul font-weight-bold">Contacto</h5>
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Solicitud de programación para citas.</td>
-                                    <td><a href="mailto:orden@ssobq.com">orden@ssobq.com</a></td>
-                                </tr>
-                                <tr>
 
-                                    <td>Solicitud o dudas en los resultados.</td>
-                                    <td><a href="mailto:resultados@ssobq.com">resultados@ssobq.com</a></td>
-                                </tr>
-                                <tr>
-                                    <td>Atención VIP y casos particulares del servicio.</td>
-                                    <td><a href="mailto:servicios@ssobq.com">servicios@ssobq.com</a></td>
-                                </tr>
-                                <tr>
-                                    <td>Solicitud certificaciones comerciales, información legal,actividades y jornadas de salud.</td>
-                                    <td><a href="mailto:administrativa@ssobq.com">administrativa@ssobq.com</a></td>
-                                </tr>
-                                <tr>
-                                    <td>Cotizaciones e información de nuestros servicios.</td>
-                                    <td><a href="mailto:mercadeo@ssobq.com">mercadeo@ssobq.com</a></td>
-                                </tr>
-                                <tr>
-                                    <td>Facturas y envío de soportes de pago.</td>
-                                    <td><a href="mailto:contabilidad@ssobq.com">contabilidad@ssobq.com</a></td>
-                                </tr>
-                                <tr>
-                                    <td>Atención red nacional.</td>
-                                    <td><a href="mailto:regional@ssobq.com">regional@ssobq.com</a></td>
-                                </tr>
-                                <tr>
-                                    <td>Atencion de quejas, sugerencias y comentarios.</td>
-                                    <td><a href="mailto:procesos@ssobq.com">procesos@ssobq.com</a></td>
-                                </tr>
-                            </tbody>
-                        </table>
+                    <div class="col-lg-12">
+                        <div class="bd-example">
+                            <table class="table table-striped table-hover ">
+                                <thead>
+                                    <tr>
+                                        <th scope="col" class="text-center th text-light">
+                                            <h4>Departamentos</h4>
+                                        </th>
+                                        <th scope="col" class="text-center th text-light">
+                                            <h4>Tipo de Solicitud</h4>
+                                        </th>
+                                        <th scope="col" class="text-center th text-light">
+                                            <h4>Persona Contacto</h4>
+                                        </th>
+                                        <th scope="col" class="text-center th text-light">
+                                            <h4>Contacto</h4>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody class="text-center">
+                                    <tr>
+                                        <td>Admisiones</td>
+                                        <td>Solicitud de programación para citas.</td>
+                                        <td>Brandon Navarro<br><p class="colorAzul">Coordinador de Admisiones</p></td>
+                                        <td><a href="mailto:orden@ssobq.com" class="text-dark">orden@ssobq.com</a><br>301 522 0695 - 360 3103 ext.113</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Conceptos</td>
+                                        <td>Solicitud o dudas en los resultados.</td>
+                                        <td>Karen Guerra<br><p class="colorAzul">Gestora de Servicios</p></td>
+                                        <td><a href="mailto:resultados@ssobq.com" class="text-dark">resultados@ssobq.com</a><br>304 272 5330 - 360 3103 ext.108</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Servicios</td>
+                                        <td>Atención VIP y casos particulares del servicio.</td>
+                                        <td>Adriana Leal<br><p class="colorAzul">Directora de Servicios</p></td>
+                                        <td><a href="mailto:dir.servicios@ssobq.com" class="text-dark">dir.servicios@ssobq.com</a><br>313 546 4664 - 360 3103 ext.107</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Administrativa y Financiera</td>
+                                        <td>Solicitud certicaciones comerciales, información legal.</td>
+                                        <td>Liliana Segrera<br><p class="colorAzul">Directora Administrativa y Financiera</p></td>
+                                        <td><a href="mailto:administrativa@ssobq.com" class="text-dark">administrativa@ssobq.com</a><br>310 424 0457 - 360 3103 ext.105</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Gestion comercial </td>
+                                        <td>Cotizaciones e información de nuestros servicios, actividades y jornadas de salud</td>
+                                        <td>Alexandra Molinares<br><p class="colorAzul">Gestor de Comercial</p></td>
+                                        <td><a href="mailto:gestioncomercial@ssobq.com" class="text-dark">gestioncomercial@ssobq.com</a><br>315 360 3621 - 360 3103 ext.115</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Cartera</td>
+                                        <td>Estados de cuenta y envío de soportes de pago</td>
+                                        <td>Maiden Ojeda<br><p class="colorAzul">Asistente Administrativa y Financiera</p></td>
+                                        <td><a href="mailto:cartera@ssobq.com" class="text-dark">cartera@ssobq.com</a><br>304 272 5329 - 360 3103 ext.115</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Regionales</td>
+                                        <td>Atención red nacional</td>
+                                        <td>Keilly Schoonewol<br><p class="colorAzul">Atención al Usuario</p></td>
+                                        <td><a href="mailto:regional@ssobq.com" class="text-dark">regional@ssobq.com</a><br>314 616 2308 - 360 3103 ext.104</td>
+
+                                    </tr>
+                                    <tr>
+                                        <td>Procesos y Desarrollo Corporativo</td>
+                                        <td>Atención de quejas sugerencias y comentarios.</td>
+                                        <td>Estefanny Jimenez<br><p class="colorAzul">Directora de Procesos y Desarrollo Corporativo</p></td>
+                                        <td><a href="mailto:procesos@ssobq.com" class="text-dark">procesos@ssobq.com</a><br>313 546 4664 - 360 3103</td>
+                                    </tr>
+                                </tbody>
+
+                            </table>
+                        </div>
                     </div>
-                    <div class="col-lg-2"></div>
                 </div>
             </div>
         </div>
