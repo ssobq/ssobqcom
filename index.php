3320eb886e32d8d93682d420211894e6ee51b184
ssobq <user.name>
2024-01-29 14:03:16 -0500

    favicon

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="img/logo.ico" />
+    <link rel="shortcut icon" href="img/favicon.ico" />
     <link rel="shortcut icon" href="img/favicon.png">
 
     <!-- Bootstrap CSS -->
