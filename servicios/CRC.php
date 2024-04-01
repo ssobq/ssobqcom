475c9e4b2ab56c57ac08f5a266899b1f7a9c7a7a
ssobq <user.name>
2024-04-01 11:40:35 -0500

    Servicio CRC

---
+++ servicios/CRC.php
@@ -0,0 +1,169 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="/img/logo.ico" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- Personalizado CSS -->
+    <link rel="stylesheet" href="/css/stylo.css">
+
+
+    <!-- Iconos font-awesome -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <title>CRC | SSO</title>
+</head>
+
+<body>
+
+
+    <?php
+    include '../html/nav.html';
+    ?>
+
+    <img class="img-fluid rounded center " src="/img/servicios/CRC/CRC.png">
+    <!-- contenido de la pagina -->
+    <div class="container">
+
+        <!-- FILA 1-->
+        <br><br>
+        <!-- FILA 2-->
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <img class="img-fluid rounded center imgCRC" src="/img/servicios/CRC/Psicosensometrica.png">
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="tituloExamCRC text-center">
+                            <h6 class="h6CRC">Psicología y coordinación motriz</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <img class="img-fluid rounded center imgCRC" src="/img/servicios/CRC/optometria.png">
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="tituloExamCRC text-center">
+                            <h6 class="h6CRC">Optometría</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <img class="img-fluid rounded center imgCRC" src="/img/servicios/CRC/Audiometria.png">
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="tituloExamCRC text-center">
+                            <h6 class="h6CRC">Fonoaudiología</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <img class="img-fluid rounded center imgCRC" src="/img/servicios/CRC/Examen-medico.png">
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="tituloExamCRC text-center">
+                            <h6 class="h6CRC">Medicina General</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <hr class="hrCRC">
+    </footer-->
+    <!-- Footer -->
+    <footer class="py-1">
+        <!-- Footer Links -->
+        <div class="container">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-4 text-center">
+                    <p>
+                        <img src="/img/iconos/Direccion.svg" width="39px" alt="Redes sociales SSO - CRC">
+                        <a href="https://www.google.com/maps?ll=10.997675,-74.806859&z=14&t=m&hl=es&gl=CO&mapclient=embed&cid=599152855593005199" target="_blank" class="colorAzul text-primary">
+                            &nbsp; &nbsp;Cra. 49 No. 74-77/91, Barranquilla / Colombia</a>
+                    </p>
+                </div>
+
+                <!-- Grid column -->
+                <div class="col-4 text-center">
+
+                    <p>
+                        <img src="/img/iconos/Telefono.svg" width="39px" alt="Redes sociales SSO - CRC">
+                        <a class="colorAzul agendaCitaCRC" href="tel:+573157400411">&nbsp; &nbsp;AGENDA TU CITA</a>
+                    </p>
+                </div>
+
+                <div class="col-4 text-center ">
+
+                    <div class="row">
+                        <a href="mailto:administracioncrc@ssobq.com" class="colorAzul">E-mail: administracioncrc@ssobq.com</a>
+                    </div>
+                    <div class="row colorAzul">
+                        <a class="colorAzul" href="tel:+576053603103">Tel. 6053603103</a>&nbsp;/&nbsp;<a class="colorAzul" href="tel:+573157400411">Cel. 3157400411</a>&nbsp;-&nbsp;
+                        <a class="colorAzul" href="tel:+573135464664">3135464664</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+        </div>
+        <!-- Footer Links -->
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-1">© 2022 Todos los derechos reservados, Copyright:
+            <a class="font-weight-bold colorAzul" href="https://ssobq.com"> ssobq.com</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+
+    <!-- estilo css para nav -->
+    <style type="text/css">
+        #serviciosNav {
+            color: #17529B !important;
+        }
+    </style>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
