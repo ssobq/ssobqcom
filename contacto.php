d629b1bf2c54cd2bee97b66d0a79163ec38f0bed
ssobq <ssomantenimiento@gmail.com>
2026-01-21 07:01:38 -0500

    Cargo Daniel Badillo

--- contacto.php
+++ contacto.php
@@ -149,7 +149,7 @@
                                     <tr>
                                         <td>Administrativa y Financiera</td>
                                         <td>Solicitud certificaciones comerciales, información legal.</td>
-                                        <td>Daniel Badillo<br><p class="colorAzul">Director Administrativa y Financiera</p></td>
+                                        <td>Daniel Badillo<br><p class="colorAzul">Director Administrativo y Financiero</p></td>
                                         <td><a href="mailto:administrativa@ssobq.com" class="text-dark">administrativa@ssobq.com</a><br>310 424 0457 - (605) 332 7294 ext.105</td>
                                     </tr>
                                     <tr>
